68e857ce3ff64d59c0700808928b8917a1916250
daniel <daniel@speakeasy.com>
2025-12-02 16:11:28 +0000

    feat: switch to dual module format

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -73,8 +73,8 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "1.0",
-  sdkVersion: "2.81.0",
-  genVersion: "2.767.2",
+  sdkVersion: "2.82.0",
+  genVersion: "2.768.0",
   userAgent:
-    "speakeasy-sdk/typescript 2.81.0 2.767.2 1.0 @unified-api/typescript-sdk",
+    "speakeasy-sdk/typescript 2.82.0 2.768.0 1.0 @unified-api/typescript-sdk",
 } as const;
